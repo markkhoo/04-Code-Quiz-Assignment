07aa67a4fc2f11b1c7487cf9dee169ef705556c7
markeezy <markkhoo95@gmail.com>
2021-03-28 20:11:51 -0700

    add html skeleton and quiz object

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+
+// Quiz Questions and Answers
+var quizes = {
+    set1: {
+        question: "question",
+        answers: {
+            answerCorrect: "asd",
+            answerWrong02: "asd",
+            answerWrong03: "asd",
+            answerWrong04: "asd"
+        }
+    },
+    set2: {
+        question: "question",
+        answers: {
+            answerCorrect: "asd",
+            answerWrong02: "asd",
+            answerWrong03: "asd",
+            answerWrong04: "asd"
+        }
+    },
+    set3: {
+        question: "question",
+        answers: {
+            answerCorrect: "asd",
+            answerWrong02: "asd",
+            answerWrong03: "asd",
+            answerWrong04: "asd"
+        }
+    }
+}
