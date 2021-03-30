30cc4150a302581aafd8e1422dbf5f927c301dd5
markeezy <markkhoo95@gmail.com>
2021-03-29 18:51:39 -0700

    add timer and game start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
+// ALL Variables
 // Pointers
+var gameStart = document.getElementById("gameStart");
+var timerSpan = document.getElementById("Timer");
 var quizChoice = document.querySelector("#quizChoices");
 var quizQuestion = document.querySelector("#quizQuestion");
 
@@ -33,11 +36,18 @@ var quizes = {
     }
 };
 
-// functions
+// Other Global Variables
+var gameOver = true;
+var globalTimer = 0;
+var currentScore = 0;
+
+// ALL Functions
+// Render Question for id="quizQuestion"
 function renderQuestion(content) {
     document.getElementById("quizQuestion").innerHTML = content; 
-}
+};
 
+// Render Choices with buttons as children for id="quizChoices"
 function renderListItems(content) {
     var li = document.createElement("li");
     li.textContent = content;
@@ -48,6 +58,36 @@ function renderListItems(content) {
     li.appendChild(button);
     quizChoice.appendChild(li);
 };
+
+// Start Timer
+function countdown() {
+    timerSpan.textContent = "00:0" + Math.floor(globalTimer / 60) + ":" + Math.floor((globalTimer % 60)/10) + (globalTimer % 10);;
+    var timeInterval = setInterval(function() {
+        globalTimer--;
+        timerSpan.textContent = "00:0" + Math.floor(globalTimer / 60) + ":" + Math.floor((globalTimer % 60)/10) + (globalTimer % 10);
+        if (globalTimer <=0) {
+            clearInterval(timeInterval);
+            gameOver = true;
+        }
+    }, 1000);
+};
+
+// INITIALIZE
+
+// GAME START
+gameStart.addEventListener("click", function(event) {
+    // Start Settings
+    event.preventDefault();
+    gameOver = false;
+    globalTimer = 120;
+    // Hide Button
+
+
+    // Start Timer
+    countdown();
+
+    console.log("Game Start Test");
+});
 
 // TESTS
 renderQuestion("Test Question");
