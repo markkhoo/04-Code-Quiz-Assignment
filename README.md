1d50d7b734aa170a4658eac4b36ac1a83c0203dc
markeezy <markkhoo95@gmail.com>
2021-03-30 22:26:55 -0700

    add ReadMe and replace question sets with real questions

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Code-Quiz-Assignment
+
+Welcome to a fun little quiz game project. Can you get the highscore?
+
+## Getting Started
+Play the quiz game [here](https://markkhoo.github.io/Code-Quiz-Assignment/). Click `Start Game` the play the quiz game and `View Highscores` to see previous scores.
+
+Rules of the game:
+* The quiz must be completed in 2 minutes otherwise the game is over.
+* Answer questions correctly to earn 100 points and 5 seconds on your timer.
+* Incorrect answers will reduce your timer by 15 seconds.
+* If all questions are answered before the timer runs out, the left over time in seconds is added to your score.
+
+When the game is over, input your in-game name to save your score.
--- assets/js/quiz-objects.js
+++ assets/js/quiz-objects.js
@@ -1,18 +1,18 @@
 // Quiz Questions and Answers
 var quizes = [
     {
-        question: "question1",
-        answer: "Correct",
-        choices: ["Correct", "1 Wrong 1", "1 Wrong 2", "1 Wrong 3"]
+        question: "What was the first U.S. state?",
+        answer: "Delaware",
+        choices: ["Delaware", "Virginia", "New York", "Massachusetts"]
     },
     {
-        question: "question2",
-        answer: "Correct",
-        choices: ["Correct", "2 Wrong 1", "2 Wrong 2", "2 Wrong 3"]
+        question: "Hg is the chemical symbol of which element?",
+        answer: "Mercury",
+        choices: ["Mercury", "Helium", "Magnesium", "Holmium"]
     },
     {
-        question: "question3",
-        answer: "Correct",
-        choices: ["Correct", "3 Wrong 1", "3 Wrong 2", "3 Wrong 3"]
+        question: "What language has the most words?",
+        answer: "English",
+        choices: ["English", "Russian", "Spanish", "Chinese"]
     }
 ];
